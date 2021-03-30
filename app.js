378c652fe77ab15cf59c893d25fedc80ff5ebdf7
Andrea Mikula <dealibros88@gmail.com>
2021-03-30 19:34:46 +0200

    Finish Portfolio

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
 
-burger.addEventlistener("click", () => {
-  nav.classList.toogle("nav-open");
-  burger.classList.toggle("toogle");
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+  burger.classList.toggle("toggle");
 });
 
 links.forEach((link) => {
-  link.addEventlistener("click", () => {
-    nav.classList.toogle("nav-open");
-    burger.classList.toggle("toogle");
+  link.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    burger.classList.toggle("toggle");
   });
 });
+
+$("ul.image-list img").centerImage();
